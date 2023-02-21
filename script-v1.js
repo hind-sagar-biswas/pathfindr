550ebac94b8a915412c18e6849e4ca043f7e858d
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-02-22 02:45:00 +0600

    restructured for future changes | debug left

---
+++ script-v1.js
@@ -0,0 +1,423 @@
+const graphNode = document.getElementById("graph");
+const screenWidth = screen.width;
+
+let graph = new Array();
+let dfsGraph = new Array();
+let grid = [0, 0];
+let dfsGrid = [0, 0];
+let startVal = 0;
+let targetVal = 1;
+let start = [0, 0];
+let found = false;
+
+let selector = "blocks";
+
+let routes = new Array();
+routes.push([]);
+routes[0].push(start);
+
+//graph
+function graphGen() {
+	let rowCount = grid[0];
+	let colCount = grid[1];
+	let cellCount = 0;
+	let generatedGraph = [];
+	for (let row_i = 0; row_i < rowCount; row_i++) {
+		let generatedRow = [];
+		for (let col_i = 0; col_i < colCount; col_i++) {
+			cellCount++;
+			generatedRow.push([cellCount, false]);
+		}
+		generatedGraph.push(generatedRow);
+	}
+	return generatedGraph;
+}
+function dfsGraphGen() {
+	dfsGrid = [Math.ceil(grid[0] / 2), Math.ceil(grid[1] / 2)];
+
+	let rowCount = dfsGrid[0];
+	let colCount = dfsGrid[1];
+	let nodeID = 0;
+	const prevNode = 0;
+	let generatedGraph = [];
+	for (let row_i = 0; row_i < rowCount; row_i++) {
+		let generatedRow = [];
+		for (let col_i = 0; col_i < colCount; col_i++) {
+			nodeID++;
+			generatedRow.push([nodeID, false, prevNode, [row_i * 2, col_i * 2]]); // nodeID (arr), marked (bool), prevNode (arr), correspondingNode (arr)
+		}
+		generatedGraph.push(generatedRow);
+	}
+	return generatedGraph;
+}
+function toggleBlockage(rowIndex, colIndex, id) {
+	if (id == startVal || id == targetVal) return;
+	switch (selector) {
+		case "start":
+			startVal = id;
+			start[0] = rowIndex;
+			start[1] = colIndex;
+			graph[rowIndex][colIndex][1] = false;
+			break;
+
+		case "end":
+			targetVal = id;
+			graph[rowIndex][colIndex][1] = false;
+			break;
+
+		default:
+			graph[rowIndex][colIndex][1] = !graph[rowIndex][colIndex][1];
+			break;
+	}
+	drawGraph();
+}
+function switchSelector() {
+	selector = document.forms["selector"]["toggler"].value;
+}
+
+// get adjacent function
+function getAdj(adjRoute) {
+	const adjRouteEndPoint = adjRoute.at(-1);
+	let tempAdjPoints = [
+		[adjRouteEndPoint[0] - 1, adjRouteEndPoint[1]],
+		[adjRouteEndPoint[0], adjRouteEndPoint[1] - 1],
+		[adjRouteEndPoint[0], adjRouteEndPoint[1] + 1],
+		[adjRouteEndPoint[0] + 1, adjRouteEndPoint[1]],
+	];
+	tempAdjPoints = shuffle(tempAdjPoints);
+	return filterAdjs(adjRoute, tempAdjPoints);
+}
+function filterAdjs(routeOfAdjs, adjsToFilter) {
+	const solidRoute = getSolidRoute(routeOfAdjs);
+	let tempFilteredAdjs = [];
+	let filteredAdjs = [];
+	adjsToFilter.forEach((adj) => {
+		if (
+			adj[0] >= 0 &&
+			adj[0] <= grid[0] - 1 &&
+			adj[1] >= 0 &&
+			adj[1] <= grid[1] - 1 &&
+			!graph[adj[0]][adj[1]][1]
+		) {
+			tempFilteredAdjs.push(adj);
+		}
+	});
+	tempFilteredAdjs.forEach((tempFilteredAdj) => {
+		const adjVal = graph[tempFilteredAdj[0]][tempFilteredAdj[1]][0];
+		if (!solidRoute.includes(adjVal)) {
+			filteredAdjs.push(tempFilteredAdj);
+		}
+	});
+	return filteredAdjs;
+}
+const getSolidRoute = (liquidRoute) => {
+	let solidRouteResult = [];
+	liquidRoute.forEach((liquidRouteCell) => {
+		solidRouteResult.push(graph[liquidRouteCell[0]][liquidRouteCell[1]][0]);
+	});
+	return solidRouteResult;
+};
+
+function getDfsAdj(currentNode) {
+	let tempAdjPoints = [
+		[currentNode[0] - 1, currentNode[1]],
+		[currentNode[0], currentNode[1] - 1],
+		[currentNode[0], currentNode[1] + 1],
+		[currentNode[0] + 1, currentNode[1]],
+	];
+	tempAdjPoints = shuffle(tempAdjPoints);
+	return filterDfsAdjs(tempAdjPoints);
+}
+function filterDfsAdjs(adjsToFilter) {
+	let filteredAdjs = [];
+	adjsToFilter.forEach((adj) => {
+		if (
+			adj[0] >= 0 &&
+			adj[0] <= dfsGrid[0] - 1 &&
+			adj[1] >= 0 &&
+			adj[1] <= dfsGrid[1] - 1
+		) {
+			if (!dfsGraph[adj[0]][adj[1]][1]) filteredAdjs.push(adj);
+		}
+	});
+	return filteredAdjs;
+}
+
+// Route creation
+function createNewRoute(currentRoute) {
+	const formedRoutes = [];
+	const currentRouteAdjs = getAdj(currentRoute);
+	currentRouteAdjs.forEach((currentRouteAdj) => {
+		const newFormedRoute = new Array();
+		currentRoute.forEach((elem) => {
+			newFormedRoute.push(elem);
+		});
+		newFormedRoute.push(currentRouteAdj);
+		formedRoutes.push(newFormedRoute);
+	});
+	return formedRoutes;
+}
+function filterRoutes(routesToFilter) {
+	let filteredRoutes = [];
+	let sameEndSets = {};
+	routesToFilter.forEach((routeToFilter) => {
+		const routeEndPointArr = routeToFilter.at(-1);
+		const routeEndpointKey = `${routeEndPointArr[0]}-${routeEndPointArr[1]}`;
+		if (
+			!Object.hasOwnProperty.call(sameEndSets, routeEndpointKey) ||
+			sameEndSets[routeEndpointKey].length > routeToFilter.length
+		) {
+			sameEndSets[routeEndpointKey] = routeToFilter;
+		}
+	});
+	for (const endPointKey in sameEndSets) {
+		if (Object.hasOwnProperty.call(sameEndSets, endPointKey)) {
+			const filteredRoute = sameEndSets[endPointKey];
+			filteredRoutes.push(filteredRoute);
+		}
+	}
+	return filteredRoutes;
+}
+
+// DFS
+function DFS(node = [0, 0], prevNode = [0, 0]) {
+	dfsGraph[node[0]][node[1]][1] = true;
+	if (!dfsGraph[node[0]][node[1]][2]) dfsGraph[node[0]][node[1]][2] = prevNode;
+	const nextNodes = getDfsAdj(node);
+	nextNodes.forEach((nextNode) => {
+		return DFS(nextNode, node);
+	});
+}
+function implementDFS() {
+	dfsGraph.forEach((row, rowIndex) => {
+		row.forEach((col, colIndex) => {
+			const graphCell = col[3];
+			const prevCell = dfsGraph[col[2][0]][col[2][1]][2];
+			const edgeCell = getEdge(graphCell, prevCell);
+
+			graph[graphCell[0]][graphCell[1]][1] = true;
+			graph[edgeCell[0]][edgeCell[1]][1] = true;
+
+			drawGraph();
+		});
+	});
+}
+function getEdge(currentNode, prevNode) {
+	const edgeRow = Math.ceil((currentNode[0] + prevNode[0]) / 2);
+	const edgeCol = Math.ceil((currentNode[1] + prevNode[1]) / 2);
+	// console.log("current node", currentNode);
+	// console.log("previous node", prevNode);
+	// console.log(edgeRow, edgeCol);
+	return [edgeRow, edgeCol];
+}
+
+// Main function
+function main(oldRoutes) {
+	let routeFound = false;
+	let newRoutes = [];
+	oldRoutes.every((route) => {
+		const newRoute = createNewRoute(route);
+		newRoute.every((nRoute) => {
+			const routeEndpoint = getSolidRoute(nRoute).at(-1);
+
+			if (routeEndpoint == targetVal) {
+				routeFound = true;
+				newRoutes = [nRoute];
+				return false;
+			}
+
+			newRoutes.push(nRoute);
+			return true;
+		});
+		if (routeFound) return false;
+		else return true;
+	});
+	newRoutes = filterRoutes(newRoutes);
+	if (newRoutes.length == 1 && getAdj(newRoutes[0]).length == 0)
+		return [true, newRoutes];
+	return [routeFound, newRoutes];
+}
+
+// Visualizers
+function drawGraph() {
+	while (graphNode.firstChild) {
+		graphNode.removeChild(graphNode.lastChild);
+	}
+
+	graph.forEach((row, rowIndex) => {
+		const rowElement = document.createElement("div");
+		rowElement.className = "row";
+		rowElement.id = `row-${rowIndex}`;
+		graphNode.appendChild(rowElement);
+
+		row.forEach((cell, cellIndex) => {
+			let color = "#567d46";
+			let value = "";
+
+			if (cell[1]) color = "#333333";
+			if (startVal == cell[0]) {
+				color = "green";
+				value = `<i class="fas fa-street-view" style="color: white"></i>`;
+			} else if (targetVal == cell[0]) {
+				color = "red";
+				value = `<i class="fas fa-flag" style="color: white"></i>`;
+			}
+
+			const cellElement = document.createElement("div");
+			cellElement.className = "cell";
+			cellElement.id = `cell-${cell[0]}`;
+			cellElement.style.backgroundColor = color;
+			cellElement.innerHTML = value;
+			document.getElementById(`row-${rowIndex}`).appendChild(cellElement);
+			document
+				.getElementById(`cell-${cell[0]}`)
+				.addEventListener("click", () => {
+					toggleBlockage(rowIndex, cellIndex, cell[0]);
+				});
+		});
+	});
+
+	if (screenWidth <= 500) {
+		graphNode.style.width = `${screenWidth - 40}px`;
+		graphNode.style.height = `${screenWidth - 40}px`;
+
+		const cellSide = (screenWidth - 40) / grid[0];
+		const cellWidth = (screenWidth - 40) / grid[1];
+		console.log(cellSide);
+		const cellList = document.getElementsByClassName("cell");
+
+		for (let cellCount = 0; cellCount < cellList.length; cellCount++) {
+			const cellElement = cellList[cellCount];
+			cellElement.style.height = `${cellSide}px`;
+			cellElement.style.width = `${cellWidth}px`;
+		}
+	}
+}
+function routeColor(routeList, mainRoute = false) {
+	routeList.forEach((rt) => {
+		let baseTime = -100;
+		let timeInterval = 100;
+		let color = "lightgreen";
+		let borderColor = "green";
+		if (mainRoute) {
+			color = "green";
+			borderColor = "darkgreen";
+			baseTime = rt.length * 100;
+		}
+		rt.forEach((r) => {
+			let time = baseTime + timeInterval;
+			const id = graph[r[0]][r[1]][0];
+			setTimeout(() => {
+				document.getElementById(`cell-${id}`).style.backgroundColor = color;
+				document.getElementById(`cell-${id}`).style.borderColor = borderColor;
+			}, time);
+			baseTime = time;
+		});
+	});
+	if (found) {
+		let solid = getSolidRoute(routeList[0]);
+		if (solid.at(-1) == targetVal)
+			document.getElementById("route").textContent = JSON.stringify(solid);
+		else
+			document.getElementById("route").textContent = "no valid routes found!";
+	}
+}
+
+// Executors
+function generate() {
+	graphNode.style.pointerEvents = "all";
+
+	// Reset
+	graph = [];
+	grid = [0, 0];
+	startVal = 0;
+	targetVal = 1;
+	start = [0, 0];
+	found = false;
+
+	// Get values
+	grid[0] = parseInt(document.getElementById("row").value);
+	grid[1] = parseInt(document.getElementById("col").value);
+	start[0] = parseInt(document.getElementById("row").value) - 1;
+	start[1] = parseInt(document.getElementById("col").value) - 1;
+	graph = graphGen();
+	startVal = graph[start[0]][start[1]][0];
+
+	// Draw graph
+	drawGraph();
+
+	// Enable buttons
+	document.getElementById("runner").disabled = false;
+	document.getElementById("runner-once").disabled = false;
+
+	// Set value of routes
+	routes.length = 0;
+	routes.push([]);
+	routes[0].push(start);
+
+	return false;
+}
+function runOnce() {
+	if (found) return 0;
+	[found, routes] = main(routes);
+	routeColor(routes);
+	document.getElementById("route").innerHTML = "";
+	routes.forEach(
+		(el) =>
+			(document.getElementById("route").innerHTML += JSON.stringify(
+				getSolidRoute(el)
+			))
+	);
+}
+function run() {
+	graphNode.style.pointerEvents = "none";
+
+	var startTime = performance.now();
+	while (!found) {
+		[found, routes] = main(routes);
+		routeColor(routes);
+	}
+	var endTime = performance.now();
+
+	drawGraph();
+	routeColor(routes, true);
+	document.getElementById("time").textContent = `took ${(
+		(endTime - startTime) /
+		1000
+	).toFixed(3)} seconds to find!`;
+}
+function shuffle(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+
+	return array;
+}
+
+function runDFS() {
+	dfsGraph = dfsGraphGen();
+	DFS();
+	implementDFS();
+}
+
+// Executions
+window.addEventListener("load", () => {
+	// For mobile screens
+	if (screen.width <= 500) {
+		const mainCont = document.getElementById("container");
+		const routeCont = document.getElementById("route-cont");
+		mainCont.style.height = `${
+			mainCont.offsetHeight + routeCont.offsetHeight * 3 + screenWidth + 100
+		}px`;
+		mainCont.style.padding = "5px";
+		console.log(mainCont.offsetHeight);
+	}
+});
--- script.js
+++ script.js
@@ -1,74 +1,64 @@
+/* GRAPH structure ****************************************************
+graph => Object(
+	key => int cellId
+	value => Object(
+		"blocked": bool false
+		"destination": bool false [if true -> 'blocked' can't be true]
+		"adjacents": Array(
+			int cellId(s)
+		)
+	)
+)
+***********************************************************************/
+// CONSTANTS
 const graphNode = document.getElementById("graph");
 const screenWidth = screen.width;
 
-let graph = [];
-let grid = [0, 0];
-let startVal = 0;
-let targetVal = 1;
-let start = [0, 0];
-let found = false;
+const graph = new Object();
+const grid = [0, 0];
 
+
+// VARIABLES
+let start = 0; // Initial position
+let found = false; // whether the destination is found or not
 let selector = "blocks";
 
 let routes = new Array();
 routes.push([]);
-routes[0].push(start);
+routes[0].push(toString(start));
 
-//graph
+
+
+// GENERATORS
 function graphGen() {
-	let rowCount = grid[0];
-	let colCount = grid[1];
-	let cellCount = 0;
-	let generatedGraph = [];
-	for (let row_i = 0; row_i < rowCount; row_i++) {
-		let generatedRow = [];
-		for (let col_i = 0; col_i < colCount; col_i++) {
-			cellCount++;
-			generatedRow.push([cellCount, false]);
-		}
-		generatedGraph.push(generatedRow);
+	const nodeCount = grid[0] * grid[1];
+
+	graph.length = 0;
+	for (let nodeID = 0; nodeID < nodeCount; nodeID++) {
+		const nodeObj = new Object();
+		nodeObj.blocked = false;
+		nodeObj.destination = (nodeID == nodeCount - 1) ? true : false;
+		nodeObj.adjacents = getAdj(nodeID);
+		graph[`${nodeID}`] = nodeObj;
 	}
-	return generatedGraph;
-}
-function toggleBlockage(rowIndex, colIndex, id) {
-	if (id == startVal || id == targetVal) return;
-	switch (selector) {
-		case "start":
-			startVal = id;
-			start[0] = rowIndex;
-			start[1] = colIndex;
-			graph[rowIndex][colIndex][1] = false;
-			break;
-
-		case "end":
-			targetVal = id;
-			graph[rowIndex][colIndex][1] = false;
-			break;
-
-		default:
-			graph[rowIndex][colIndex][1] = !graph[rowIndex][colIndex][1];
-			break;
-	}
-	drawGraph();
-}
-function switchSelector() {
-	selector = document.forms["selector"]["toggler"].value;
 }
 
-// get adjacent function
-function getAdj(adjRoute) {
-	const adjRouteEndPoint = adjRoute.at(-1);
+
+
+// get possible adjacent nodeID(s)
+function getAdj(node) {
+	const row = Math.floor(node / grid[1]);
+	const col = node - (row * grid[1]);
 	let tempAdjPoints = [
-		[adjRouteEndPoint[0] - 1, adjRouteEndPoint[1]],
-		[adjRouteEndPoint[0], adjRouteEndPoint[1] - 1],
-		[adjRouteEndPoint[0], adjRouteEndPoint[1] + 1],
-		[adjRouteEndPoint[0] + 1, adjRouteEndPoint[1]],
+		[row - 1, col],
+		[row, col - 1],
+		[row, col + 1],
+		[row + 1, col],
 	];
 	tempAdjPoints = shuffle(tempAdjPoints);
-	return filterAdjs(adjRoute, tempAdjPoints);
+	return filterAdjs(tempAdjPoints);
 }
-function filterAdjs(routeOfAdjs, adjsToFilter) {
-	const solidRoute = getSolidRoute(routeOfAdjs);
+function filterAdjs(adjsToFilter) {
 	let tempFilteredAdjs = [];
 	let filteredAdjs = [];
 	adjsToFilter.forEach((adj) => {
@@ -76,39 +66,38 @@ function filterAdjs(routeOfAdjs, adjsToFilter) {
 			adj[0] >= 0 &&
 			adj[0] <= grid[0] - 1 &&
 			adj[1] >= 0 &&
-			adj[1] <= grid[1] - 1 &&
-			!graph[adj[0]][adj[1]][1]
+			adj[1] <= grid[1] - 1
 		) {
 			tempFilteredAdjs.push(adj);
 		}
 	});
 	tempFilteredAdjs.forEach((tempFilteredAdj) => {
-		const adjVal = graph[tempFilteredAdj[0]][tempFilteredAdj[1]][0];
-		if (!solidRoute.includes(adjVal)) {
-			filteredAdjs.push(tempFilteredAdj);
-		}
+		const adjVal = (tempFilteredAdj[0] * grid[1]) + tempFilteredAdj[1];
+		filteredAdjs.push(toString(adjVal));
 	});
 	return filteredAdjs;
 }
-const getSolidRoute = (liquidRoute) => {
-	let solidRouteResult = [];
-	liquidRoute.forEach((liquidRouteCell) => {
-		solidRouteResult.push(graph[liquidRouteCell[0]][liquidRouteCell[1]][0]);
-	});
-	return solidRouteResult;
-};
+function checkAdjAvailable(node) {
+	graph[node].adjacents.forEach(adj => { if (!graph[adj].blocked) return true; });
+	return false;
+}
+
+
 
 // Route creation
 function createNewRoute(currentRoute) {
 	const formedRoutes = [];
-	const currentRouteAdjs = getAdj(currentRoute);
+	const currentRouteId = parseInt(currentRoute.at(-1));
+	const currentRouteAdjs = graph[currentRouteId].adjacents;
 	currentRouteAdjs.forEach((currentRouteAdj) => {
-		const newFormedRoute = new Array();
-		currentRoute.forEach((elem) => {
-			newFormedRoute.push(elem);
-		});
-		newFormedRoute.push(currentRouteAdj);
-		formedRoutes.push(newFormedRoute);
+		if (!graph[currentRouteAdj].blocked) {
+			const newFormedRoute = new Array();eEndPoint
+			currentRoute.forEach((node) => {
+				newFormedRoute.push(node);
+			});
+			newFormedRoute.push(currentRouteAdj);
+			formedRoutes.push(newFormedRoute);
+		}
 	});
 	return formedRoutes;
 }
@@ -116,13 +105,9 @@ function filterRoutes(routesToFilter) {
 	let filteredRoutes = [];
 	let sameEndSets = {};
 	routesToFilter.forEach((routeToFilter) => {
-		const routeEndPointArr = routeToFilter.at(-1);
-		const routeEndpointKey = `${routeEndPointArr[0]}-${routeEndPointArr[1]}`;
-		if (
-			!Object.hasOwnProperty.call(sameEndSets, routeEndpointKey) ||
-			sameEndSets[routeEndpointKey].length > routeToFilter.length
-		) {
-			sameEndSets[routeEndpointKey] = routeToFilter;
+		const routeEndPoint = routeToFilter.at(-1);
+		if (!Object.hasOwnProperty.call(sameEndSets, routeEndPoint) || sameEndSets[routeEndPoint].length > routeToFilter.length ) {
+			sameEndSets[routeEndPoint] = routeToFilter;
 		}
 	});
 	for (const endPointKey in sameEndSets) {
@@ -134,16 +119,18 @@ function filterRoutes(routesToFilter) {
 	return filteredRoutes;
 }
 
-// Main function
-function main(oldRoutes) {
+
+
+// MAIN FUNCTIONS
+function findr(routes) {
 	let routeFound = false;
 	let newRoutes = [];
-	oldRoutes.every((route) => {
+	routes.every((route) => {
 		const newRoute = createNewRoute(route);
 		newRoute.every((nRoute) => {
-			const routeEndpoint = getSolidRoute(nRoute).at(-1);
+			const routeEndpoint = nRoute.at(-1);
 
-			if (routeEndpoint == targetVal) {
+			if (graph[routeEndpoint].destination) {
 				routeFound = true;
 				newRoutes = [nRoute];
 				return false;
@@ -156,49 +143,35 @@ function main(oldRoutes) {
 		else return true;
 	});
 	newRoutes = filterRoutes(newRoutes);
-	if (newRoutes.length == 1 && getAdj(newRoutes[0]).length == 0) return [true, newRoutes];
+	if (newRoutes.length == 1 && !checkAdjAvailable(newRoutes[0].at(-1))) return [true, newRoutes];
 	return [routeFound, newRoutes];
 }
 
-// Visualizers
-function drawGraph() {
-	while (graphNode.firstChild) {
-		graphNode.removeChild(graphNode.lastChild);
-	}
 
-	graph.forEach((row, rowIndex) => {
+
+// VISUALIZERS
+function drawGraph() {
+	for (let rowCount = 0; rowCount < grid[0]; rowCount++) {
 		const rowElement = document.createElement("div");
 		rowElement.className = "row";
-		rowElement.id = `row-${rowIndex}`;
+		rowElement.id = `row-${rowCount}`;
 		graphNode.appendChild(rowElement);
-
-		row.forEach((cell, cellIndex) => {
-			let color = "#567d46";
-			let value = "";
-
-			if (cell[1]) color = "#333333";
-			if (startVal == cell[0]) {
-				color = "green";
-				value = `<i class="fas fa-street-view" style="color: white"></i>`;
-			} else if (targetVal == cell[0]) {
-				color = "red";
-				value = `<i class="fas fa-flag" style="color: white"></i>`;
+	}
+	for (const node in graph) {
+		if (Object.hasOwnProperty.call(graph, node)) {
+			const nodeObj = graph[node];
+			const rowId = `row-${Math.floor(parseInt(node) / grid[1])}`;
+			if (node != "length") {
+				const nodeElement = document.createElement("div");
+				nodeElement.className = "cell ";
+				if (parseInt(node) == start) nodeElement.className += "start ";
+				else if (nodeObj.destination) nodeElement.className += "destination ";
+				else if (nodeObj.blocked) nodeElement.className += "blocked ";
+				nodeElement.id = `cell-${node}`;
+				document.getElementById(rowId).appendChild(nodeElement);
 			}
-
-			const cellElement = document.createElement("div");
-			cellElement.className = "cell";
-			cellElement.id = `cell-${cell[0]}`;
-			cellElement.style.backgroundColor = color;
-			cellElement.innerHTML = value;
-			document.getElementById(`row-${rowIndex}`).appendChild(cellElement);
-			document
-				.getElementById(`cell-${cell[0]}`)
-				.addEventListener("click", () => {
-					toggleBlockage(rowIndex, cellIndex, cell[0]);
-				});
-		});
-	});
-
+		}
+	}
 	if (screenWidth <= 500) {
 		graphNode.style.width = `${screenWidth - 40}px`;
 		graphNode.style.height = `${screenWidth - 40}px`;
@@ -206,7 +179,7 @@ function drawGraph() {
 		const cellSide = (screenWidth - 40) / grid[0];
 		const cellWidth = (screenWidth - 40) / grid[1];
 		console.log(cellSide);
-		const cellList = document.getElementsByClassName('cell')
+		const cellList = document.getElementsByClassName("cell");
 
 		for (let cellCount = 0; cellCount < cellList.length; cellCount++) {
 			const cellElement = cellList[cellCount];
@@ -216,7 +189,7 @@ function drawGraph() {
 	}
 }
 function routeColor(routeList, mainRoute = false) {
-	routeList.forEach((rt) => {
+	routeList.forEach((route) => {
 		let baseTime = -100;
 		let timeInterval = 100;
 		let color = "lightgreen";
@@ -224,47 +197,40 @@ function routeColor(routeList, mainRoute = false) {
 		if (mainRoute) {
 			color = "green";
 			borderColor = "darkgreen";
-			baseTime = rt.length * 100;
+			baseTime = route.length * 100;
 		}
-		rt.forEach((r) => {
+		route.forEach((node) => {
 			let time = baseTime + timeInterval;
-			const id = graph[r[0]][r[1]][0];
 			setTimeout(() => {
-				document.getElementById(`cell-${id}`).style.backgroundColor = color;
-				document.getElementById(`cell-${id}`).style.borderColor = borderColor;
+				document.getElementById(`cell-${node}`).style.backgroundColor = color;
+				document.getElementById(`cell-${node}`).style.borderColor = borderColor;
 			}, time);
 			baseTime = time;
 		});
 	});
 	if (found) {
-		let solid = getSolidRoute(routeList[0]);
-		if (solid.at(-1) == targetVal)
-			document.getElementById("route").textContent = JSON.stringify(solid);
+		if (routeList[0].at(-1) == targetVal) document.getElementById("route").textContent = JSON.stringify(solid);
 		else document.getElementById("route").textContent = "no valid routes found!";
 	}
 }
 
-// Executors
+
+// EXECUTORS
 function generate() {
 	graphNode.style.pointerEvents = "all";
 
-	// Reset
-	graph = [];
-	grid = [0, 0];
-	startVal = 0;
-	targetVal = 1;
-	start = [0, 0];
+	// Resets
+	grid[0] = 0;
+	grid[1] = 0;
+	start = 0;
 	found = false;
 
 	// Get values
 	grid[0] = parseInt(document.getElementById("row").value);
 	grid[1] = parseInt(document.getElementById("col").value);
-	start[0] = parseInt(document.getElementById("row").value) - 1;
-	start[1] = parseInt(document.getElementById("col").value) - 1;
-	graph = graphGen();
-	startVal = graph[start[0]][start[1]][0];
 
-	// Draw graph
+	// Generate graph
+	graphGen();
 	drawGraph();
 
 	// Enable buttons
@@ -274,56 +240,41 @@ function generate() {
 	// Set value of routes
 	routes.length = 0;
 	routes.push([]);
-	routes[0].push(start);
+	routes[0].push(toString(start));
 
 	return false;
 }
 function runOnce() {
-	if (found) return 0;
-	[found, routes] = main(routes);
-	routeColor(routes);
-	document.getElementById("route").innerHTML = "";
-	routes.forEach(
-		(el) =>
-			(document.getElementById("route").innerHTML += JSON.stringify(
-				getSolidRoute(el)
-			))
-	);
+	grid[0] = 7;
+	grid[1] = 6;
+	graphGen();
+	drawGraph();
 }
 function run() {
 	graphNode.style.pointerEvents = "none";
 
 	var startTime = performance.now();
 	while (!found) {
-		[found, routes] = main(routes);
+		[found, routes] = findr(routes);
 		routeColor(routes);
 	}
 	var endTime = performance.now();
 
-	drawGraph();
 	routeColor(routes, true);
 	document.getElementById("time").textContent = `took ${((endTime - startTime) / 1000).toFixed(3)} seconds to find!`;
 }
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+	let currentIndex = array.length,
+		randomIndex;
 
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
-// Executions
-window.addEventListener("load", () => {
-	// For mobile screens
-	if (screen.width <= 500) {
-		const mainCont = document.getElementById("container");
-		const routeCont = document.getElementById("route-cont");
-		mainCont.style.height = `${mainCont.offsetHeight + (routeCont.offsetHeight * 3) + screenWidth + 100}px`;
-		mainCont.style.padding = "5px";
-		console.log(mainCont.offsetHeight);
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
 	}
-});
+
+	return array;
+}
